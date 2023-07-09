0831e2a47efac20fb3a241d95d4afce3a3b8dbaf
h-zahar <64719808+h-zahar@users.noreply.github.com>
2023-07-09 05:59:16 +0000

    Finalized Prototype

--- index.js
+++ index.js
@@ -8,25 +8,51 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const getDateObj = (req) => {
+  let { date } = req.params;
+  date = date === undefined ? new Date().toDateString() : date;
+
+  const strDate = new Date((date.includes('-') || date.includes(' ')) ? date : Number(date)).toDateString();
+
+  if (strDate === "Invalid Date") return { error: strDate };
+
+  const dateElems = new Date((date.includes('-') || date.includes(' ')) ? date : Number(date)).toDateString().split(' ');
+
+  const unix = new Date((date.includes('-') || date.includes(' ')) ? date : Number(date)).getTime();
+  const utc = dateElems[0].concat(', ').concat(dateElems.slice(1, dateElems.length).concat('GMT').join(' '));
+
+  const dateObj = { unix, utc };
+  return dateObj;
+
+
+};
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/', (req, res) => {
+  const dateObj = getDateObj(req);
+  res.json(dateObj);
+});
 
+app.get("/api/:date", (req, res) => {
+  const dateObj = getDateObj(req);
+  res.json(dateObj);
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
